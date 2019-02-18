0e61c79da0cf6a125a7eb6baa03893d54f234696
ultranoob-5 <anishpratheepan@gmail.com>
2019-02-18 07:31:14 +0000

    potter: Dot bring up

--- dot_potter.mk
+++ dot_potter.mk
@@ -18,16 +18,15 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/potter/full_potter.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := potter
-PRODUCT_NAME := arrow_potter
+PRODUCT_NAME := dot_potter
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
