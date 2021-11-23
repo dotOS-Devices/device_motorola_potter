bddbaa3325d59435bde8451ca31ea5a45f8d5f92
zjrdroid <priv.zjr@gmail.com>
2021-11-23 10:10:48 -0300

    Update FP to november

--- dot_potter.mk
+++ dot_potter.mk
@@ -40,8 +40,8 @@ PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := potter
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ3A.210905.001 7511028 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RD1A.201105.003/6886239:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RD1A.201105.003 6886239 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
